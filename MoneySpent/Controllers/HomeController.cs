f2ecd053c690ef66f14fd38a6e1c778c5f904643
SACHEN MALAR MARAN M <sachenm@completehealthsys.com>
2020-06-10 08:53:35 +0530

    completed home page.

--- MoneySpent/Controllers/HomeController.cs
+++ MoneySpent/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MoneySpent.Models;
 
 namespace MoneySpent.Controllers
 {
@@ -12,6 +13,19 @@ namespace MoneySpent.Controllers
         {
             return View();
         }
+
+        public ActionResult LogIn()
+        {
+            UserContext Users = new UserContext();
+
+            int id = 1;
+            //User selectedUser = Users.UserList.Single(user => user.UserId == id);
+
+            List< User> selectedUser = Users.UserList.ToList();
+
+            return View(selectedUser);
+        }
+
 
         public ActionResult Entry()
         {
---
+++ MoneySpent/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MoneySpent.Models
+{
+    [Table("UserDetails")]
+    public class User
+    {
+        public int UserId { get; set; }
+        public string F_Name { get; set; }
+        public string L_Name { get; set; }
+        public Int16 Age {  get; set; }
+    
+    }
+}
